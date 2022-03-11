d8b61e079a52bd3454f8f505835f60cc768223b2
kaylezy <kunlex_042003@yahoo.comkunlex_042003@yahoo.com>
2022-03-11 23:30:13 +0100

    alphabet in lowercase

---
+++ 0x01-variables_if_else_while/4-print_alphabet.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Description: Get a random number and check its last digit, compare it with 5
+ * Return: 0
+ */
+int main(void)
+{
+	char a = 'a';
+
+	while (a <= 'z')
+	{
+		if (a == 'e' || a == 'q')
+		{
+
+		}
+		else
+		putchar(a);
+			a++;
+	}
+
+	putchar('\n');
+	return (0);
+}
