3fe6d8b720184702015635666a147ebbb548ad3f
kaylezy <kunlex_042003@yahoo.comkunlex_042003@yahoo.com>
2022-03-11 23:35:24 +0100

    single digit numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Description: this print base 10 numbers
+ * Return: 0
+ */
+int main(void)
+{
+	int n = 0;
+
+	while (n < 10)
+	{
+		printf("%d", n);
+		n++;
+	}
+	printf(\n);
+	return (0);
+}
